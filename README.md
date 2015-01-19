569432b59976945370a4de4346658ed232f08c84
Nghia Nguyen <zinzinday@gmail.com>
2015-01-19 18:53:54 +0700

    Refactor and add locale (#1)

--- README.md
+++ README.md
@@ -2,3 +2,29 @@ yii2-timeago
 ============
 
 Timeago is a jQuery plugin that makes it easy to support automatically updating fuzzy timestamps. Forked from @rmm5t for Yii2 framework
+
+
+## Installation
+
+Either run
+```
+composer global require "fxp/composer-asset-plugin:1.0.0-beta4"
+```
+
+And add
+
+```
+"yiidoc/yii2-timeago": "dev-master",
+```
+
+## Usage
+
+```
+<?= \yii\timeago\TimeAgo::widget(['timestamp' => $model->published_at]); ?>
+```
+
+
+Now you can use the widget.
+If you have time, I will write detailed instructions.
+If you want to change or issue. please open the issue or create pull
+Thanks for use.
--- TimeAgo.php
+++ TimeAgo.php
@@ -9,31 +9,76 @@
 namespace yii\timeago;
 
 use Yii;
+use yii\base\InvalidConfigException;
 use yii\helpers\Html;
 use yii\base\Widget;
 use yii\helpers\ArrayHelper;
 
 /**
  *
- * @author Nghia Nguyen <yiidevelop@hotmail.com>
+ * @author Nghia Nguyen <zinzinday@gmail.com>
  * @since 2.0
  */
 class TimeAgo extends Widget
 {
-
-    public $options = ['class' => 'timeago'];
+    /**
+     * @var string The html tag to display data time. You can use abbr, span, time ...
+     * Default value "time"
+     */
+    public $tag = 'time';
+    /**
+     * @var array Options attribute for html tag
+     */
+    public $options;
+    /**
+     * @var string
+     * @see https://github.com/rmm5t/jquery-timeago/tree/master/locales
+     */
+    public $language = 'en';
+    /**
+     * @var integer Unix timestamp
+     */
     public $timestamp;
+
+
+    private $_assetBundle;
 
     public function init()
     {
-        TimeAgoAsset::register($this->getView());
-        $this->getView()->registerJs("jQuery('.{$this->options['class']}').timeago();", 4, 'timeago');
+        $this->options['data-toggle'] = ArrayHelper::getValue($this->options, 'data-toggle', 'timeago');
+        $this->registerLocale();
+        $this->getView()->registerJs("jQuery('{$this->tag}[data-toggle=\"{$this->options['data-toggle']}\"]').timeago();", 4, 'timeago');
+    }
+
+    protected function registerLocale()
+    {
+        if (file_exists(Yii::getAlias($this->getAssetBundle()->sourcePath) . DIRECTORY_SEPARATOR . "locales" . DIRECTORY_SEPARATOR . "jquery.timeago.{$this->language}.js")) {
+            $this->getAssetBundle()->js[] = "locales/jquery.timeago.{$this->language}.js";
+        } else {
+            throw new InvalidConfigException("Language '{$this->language}' do not exist.");
+        }
+    }
+
+    protected function registerAssetBundle()
+    {
+        $this->_assetBundle = TimeAgoAsset::register($this->getView());
+    }
+
+    /**
+     * @return TimeAgoAsset
+     */
+    public function getAssetBundle()
+    {
+        if (!$this->_assetBundle) {
+            $this->registerAssetBundle();
+        }
+        return $this->_assetBundle;
     }
 
     public function run()
     {
         if ($this->timestamp) {
-            echo Html::tag('time', Yii::$app->formatter->asDatetime($this->timestamp), ArrayHelper::merge($this->options, ['datetime' => $this->timestamp]));
+            echo Html::tag($this->tag, Yii::$app->formatter->asDatetime($this->timestamp), ArrayHelper::merge($this->options, ['datetime' => Yii::$app->formatter->asDatetime($this->timestamp)]));
         }
     }
 
